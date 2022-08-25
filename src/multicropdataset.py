f29aee8616abdee7f1a9f80819ee8ae1cb00c7a7
revbucket <mattjordan.mail@gmail.com>
2022-08-25 12:01:51 -0700

    fixed multicropdataset

--- src/multicropdataset.py
+++ src/multicropdataset.py
@@ -15,10 +15,10 @@ import torchvision.transforms as transforms
 logger = getLogger()
 
 
-class MultiCropDataset(datasets.ImageFolder):
+class MultiCropDataset(datasets.CIFAR100):
     def __init__(self, data_path, size_crops, nmb_crops,
                  size_dataset=-1, return_index=False):
-        super(MultiCropDataset, self).__init__(data_path)
+        super(MultiCropDataset, self).__init__(data_path, download=True, train=True)
         assert len(size_crops) == len(nmb_crops)
         if size_dataset >= 0:
             self.samples = self.samples[:size_dataset]
@@ -34,58 +34,47 @@ class MultiCropDataset(datasets.ImageFolder):
             crop = transforms.RandomCrop(
                 size_crops[i], padding=2,)
 
+            cutout = Cutout(4, 0.5)
 
             trans.extend([transforms.Compose([
                 transforms.RandomHorizontalFlip(p=0.5),
                 crop,
                 transforms.ToTensor(),
                 normalize,
-                cutout(4, 0.5, False)
+                cutout,
                 ])] * nmb_crops[i])
         self.trans = trans
 
-        
+
     def __getitem__(self, index):
-        path, _ = self.samples[index]
-        image = self.loader(path)
+        #path, _ = self.samples[index]
+        #image = self.loader(path)
+        image, _ = super().__getitem__(index)
+        # image, _ = self.__getitem__(index)
         multi_crops = list(map(lambda trans: trans(image), self.trans))
         if self.return_index:
             return index, multi_crops
         return multi_crops
 
 
+class Cutout:
+    def __init__(self, mask_size, p):
+        self.mask_size = mask_size
+        self.p = p # probability we do the cutout
 
-def cutout(mask_size, p, cutout_inside, mask_color=(0, 0, 0)):
-    mask_size_half = mask_size // 2
-    offset = 1 if mask_size % 2 == 0 else 0
+    def __call__(self, x):
+        # First pick a random int to start for x and y
+        if np.random.random() > self.p:
+            return x
 
-    def _cutout(image):
-        image = np.asarray(image).copy()
+        xmin = ymin = -self.mask_size
+        xmax = x.shape[1]
+        ymax = x.shape[2]
+        xstart = np.random.randint(xmin, xmax)
+        xend = xstart + self.mask_size
 
-        if np.random.random() > p:
-            return image
+        ystart = np.random.randint(ymin, ymax)
+        yend = ystart + self.mask_size
 
-        h, w = image.shape[:2]
-
-        if cutout_inside:
-            cxmin, cxmax = mask_size_half, w + offset - mask_size_half
-            cymin, cymax = mask_size_half, h + offset - mask_size_half
-        else:
-            cxmin, cxmax = 0, w + offset
-            cymin, cymax = 0, h + offset
-
-        cx = np.random.randint(cxmin, cxmax)
-        cy = np.random.randint(cymin, cymax)
-        xmin = cx - mask_size_half
-        ymin = cy - mask_size_half
-        xmax = xmin + mask_size
-        ymax = ymin + mask_size
-        xmin = max(0, xmin)
-        ymin = max(0, ymin)
-        xmax = min(w, xmax)
-        ymax = min(h, ymax)
-        image[ymin:ymax, xmin:xmax] = mask_color
-        return image
-
-    return _cutout
-
+        x[:, max(0, xstart):xend, max(0, ystart):yend].zero_()
+        return x
